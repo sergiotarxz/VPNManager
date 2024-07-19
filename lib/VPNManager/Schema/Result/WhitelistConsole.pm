31e82d4b7e2f099f395e0d2c0ead403818236fe9
sergiotarxz <sergiotarxz@posteo.net>
2024-07-19 20:07:25 +0200

    WhitelistConsole missing file.

---
+++ lib/VPNManager/Schema/Result/WhitelistConsole.pm
@@ -0,0 +1,25 @@
+package VPNManager::Schema::Result::WhitelistConsole;
+
+use v5.38.2;
+
+use strict;
+use warnings;
+
+use feature 'signatures';
+
+use parent 'DBIx::Class::Core';
+
+__PACKAGE__->table('whitelist_console');
+
+__PACKAGE__->add_columns(
+    id => {
+        data_type         => 'INTEGER',
+        is_auto_increment => 1,
+    },
+    username => {
+        data_type   => 'TEXT',
+        is_nullable => 0,
+    },
+);
+__PACKAGE__->set_primary_key('id');
+1;
